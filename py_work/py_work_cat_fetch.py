e82fcc4de3a3a5469546769d25eee4d928f834e4
庄佳涛 <zhuangjiatao@qipeipu.com>
2018-11-20 11:56:11 +0800

    udpte

--- py_work/py_work_cat_fetch.py
+++ py_work/py_work_cat_fetch.py
@@ -2,14 +2,13 @@ from bs4 import BeautifulSoup
 from urllib import request
 
 # 体积相关应用cat-dao调用数据 #
-mapper_set = set()
-
 def get_soup(url):
     req_result = request.urlopen(url);
     # req_result = request.urlopen(url)
     return BeautifulSoup(req_result.read().decode("utf-8"), "html.parser")
 
 def parser_soup(url):
+    mapper_set = set()
     soup = get_soup(url)
     trs = soup.find_all("tr", class_=" right")
     for i in range(1, len(trs)):
@@ -22,3 +21,11 @@ def parser_soup(url):
 def fetch_data(domain_name, start_date, end_date):
     url = "http://cat.qipeipu.net/cat/r/t?op=history&domain=" + domain_name + "&ip=All&reportType=month&type=DAO&sort=total&startDate=" + start_date + "&endDate=" + end_date
     return parser_soup(url)
+
+def main():
+    url = "http://cat.qipeipu.net/cat/r/t?op=history&domain=logistics-service&ip=All&reportType=month&type=PigeonService&sort=total&startDate=20181101&endDate=20181201&queryname="
+    mapper_set = parser_soup(url)
+    for mapper in mapper_set:
+        print(mapper)
+
+main()
