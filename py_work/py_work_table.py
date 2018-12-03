4547344c6edd90cb9d0d1c3dc2e31804bcce77d3
庄佳涛 <zhuangjiatao@qipeipu.com>
2018-12-03 10:27:17 +0800

    update

--- py_work/py_work_table.py
+++ py_work/py_work_table.py
@@ -99,70 +99,65 @@ tables = [
     'qp_reentry_goods'
 ]
 
-class TableStatus:
-
-    def __init__(self, table_name, status_1, status_2, status_3):
+class AppToTable:
+    def __init__(self, app_path, table_name):
+        self.app_path = app_path
         self.table_name = table_name
-        self.status_1 = status_1
-        self.status_2 = status_2
-        self.status_3 = status_3
 
 def main():
-    path_1 = "/Users/zhuangjt/Documents/tablesToApp.txt"
-    path_2 = "/Users/zhuangjt/Documents/table_tag.txt"
-    path_3 = "/Users/zhuangjt/Documents/tables.txt"
+    path_1 = "/Users/zhuangjt/Documents/logistics_tables.txt"
+    path_2 = "/Users/zhuangjt/Documents/result.txt"
 
-    with open(path_3, "r") as file:
+    logistics_tables = []
+    app_to_tables = []
+
+    with open(path_1, "r") as file:
         line = file.readline()
         while line:
-            print(line[line.index(":")+1:].strip())
-            # print(line[:line.index(':')].strip())
-            # print(line[line.index(':') + 1:].strip())
-            # var_list = line.split(":")
-            # print(var_list[2])
-            # tables.append(line[:line.index(':')].strip())
-            # for table in tables:
-            #     if line.__contains__(table):
-            #         var_set.add(line.strip())
-            #         break
+            logistics_tables.append(line.strip())
+            line = file.readline()
 
-            # print(tables)
+    with open(path_2, "r") as file:
+        line = file.readline()
+        while line:
+            var_list = line.split(":")
+            app_to_tables.append(AppToTable(var_list[0].strip(), var_list[1].strip()))
 
             line = file.readline()
 
+    for table in tables:
+        app_names = set()
 
-    # table_status_list = []
-    # with open(path_2, "r") as file:
-    #     line = file.readline()
-    #     while line:
-    #         var_list = line.split(":")
-    #         if len(var_list) > 0:
-    #             table_name = ""
-    #             status_1 = ""
-    #             status_2 = ""
-    #             status_3 = ""
-    #
-    #             for i in range(0, len(var_list)):
-    #                 if i == 0:
-    #                     table_name = var_list[i].strip()
-    #                 if i == 1:
-    #                     status_1 = var_list[1].strip()
-    #                 if i == 2:
-    #                     status_2 = var_list[2].strip()
-    #                 if i == 3:
-    #                     status_3 = var_list[3].strip()
-    #
-    #             table_status_list.append(TableStatus(table_name, status_1, status_2, status_3))
-    #
-    #         line = file.readline()
-    #
-    # for table in tables:
-    #     for table_status in table_status_list:
-    #         if table_status.table_name == table:
-    #             print(table_status.table_name + ":" + table_status.status_1 + ':'+ table_status.status_2 + ':'+ table_status.status_3+ ':')
-    # print(table_status_list)
+        for app_to_table in app_to_tables:
+            if table == app_to_table.table_name:
+                app_names.add(parse_app_name(app_to_table.app_path))
 
+        print_info(app_names, table)
 
-main()
+def parse_app_name(app_path):
+    return app_path.split("/")[3]
 
+def print_info(app_names, table):
+    app_names_str = ""
+    if len(app_names) >= 0:
+        for app_name in app_names:
+            app_names_str += app_name + "/"
+
+    app_names_str += ":" + table
+
+    print(app_names_str)
+
+def parser_line(path):
+    with open(path, "r") as file:
+        line = file.readline()
+
+        while line:
+            var_list = line.split(":")
+            print(var_list[0].strip())
+
+            line = file.readline()
+
+# main()
+
+parser_line("/Users/zhuangjt/Documents/outer_app_tables.txt")
 
